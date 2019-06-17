bf254b64c88885777029f8743a713a7bbab50596
ubu18-kanji <smile.smiling01@gmail.com>
2019-06-18 00:41:50 +0900

    add function for read_yaml

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import os
 import urllib.request
 import yaml
 
+CONFIG_PATH = "./config.yaml"
+
+
 def main():
+
+    with open(CONFIG_PATH) as f:
+        config = yaml.load(stream=f, Loader=yaml.SafeLoader)
+
+    print(config["slack"])
 
 
 if __name__ == "__main__":
