31be08cfb24417f16db4d740d69edc95afe1350f
ubu18-kanji <smile.smiling01@gmail.com>
2019-06-22 14:04:33 +0900

    mod main stream

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+import copy
 import yaml
 
 import slack_api
@@ -21,45 +22,42 @@ def main():
     member_list = slack.get_member_list()
 
     channel_analysis_list = []
-
     for channel in channel_list:
-        #print("============================")
-        #print(channel["id"], channel["name"])
-        user_frequency_dict = {member["id"]: 0 for member in member_list}
-        user_frequency_dict["OTHERS"] = 0  # add other_user etc (rss...)
+
+        # create member information list [{"id":***, "name":***, "frequency":***}, ...]
+        member_info_list = copy.deepcopy(member_list)
+        for member_info in member_info_list:
+            member_info.update({"frequency": 0})
+        member_info_list.append({"id": None, "name": "others", "frequency": 0})  # add "others" user
+
+        # add member post frequency
         channel_message_list = slack.get_channel_message_list(channel["id"])
-        #print("message num: {}".format(len(channel_message_list)))
+        for channel_message in channel_message_list:
+            # if "user" not in message, add "others" frequency
+            if not ("user" in channel_message["message"]):
+                member_info_list[-1]["frequency"] += 1
+                continue
+            # add target-user frequency
+            for member_info in member_info_list:
+                if member_info["id"] == channel_message["message"]["user"]:
+                    member_info["frequency"] += 1
+
+        # sort frequency
+        member_info_list_sorted = sorted(
+            member_info_list, key=lambda x: x["frequency"], reverse=True)
 
         channel_frequency = len(channel_message_list)
-
-        for channel_message in channel_message_list:
-            if not ("user" in channel_message["message"]):
-                user_frequency_dict["OTHERS"] += 1
-                continue
-            user_frequency_dict[channel_message["message"]["user"]] += 1
-
-        user_frequency_sorted = sorted(user_frequency_dict.items(), key=lambda x: x[1], reverse=True)
-        #print(user_frequency_sorted)
 
         channel_analysis = {
             "channel": channel["name"],
             "channel_frequency": channel_frequency,
-            "user_frequency": user_frequency_sorted,
+            "user_frequency": member_info_list_sorted,
         }
         channel_analysis_list.append(channel_analysis)
 
     channel_analysis_list_sorted = sorted(channel_analysis_list, key=lambda x: x["channel_frequency"], reverse=True)
 
-    fields_list = []
-    for channel_analysis in channel_analysis_list_sorted[:5]:
-        user_frequency = channel_analysis["user_frequency"]
-
-        fields_list.append({
-            "title": "1st: {}".format(channel_analysis["channel"]),
-            "value": "frequency_user: {}".format()
-        })
-
-    #slack.post_message(message=channel_analysis_list)
+    slack.post_message(message=channel_analysis_list_sorted)
 
 
 if __name__ == "__main__":
