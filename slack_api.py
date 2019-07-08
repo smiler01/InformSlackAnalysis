0488ec88af9f164c121064367fb379fa476e3ffb
ubu18-kanji <smile.smiling01@gmail.com>
2019-07-08 19:04:22 +0900

    mod post_message process

--- slack_api.py
+++ slack_api.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import requests
+import json
 import time
 
 
@@ -10,7 +11,7 @@ class SlackAPI(object):
         self.OAUTH_ACCESS_TOKEN = oauth_access_token
         self.BOT_USER_OAUTH_ACCESS_TOKEN = bot_user_oauth_access_token
         self.LATEST = time.time()
-        self.OLDEST = time.time() - 60*60*24*30
+        self.OLDEST = time.time() - 60*60*24*7
 
     def get_member_list(self):
 
@@ -35,9 +36,9 @@ class SlackAPI(object):
         url = "https://slack.com/api/channels.history"
         payload = {"token": self.OAUTH_ACCESS_TOKEN,
                    "channel": channel_id,
-                   "count": "{}".format(1000)}
-                   #"latest": "{}".format(self.LATEST),
-                   #"oldest": "{}".format(self.OLDEST)}
+                   "count": "{}".format(1000),
+                   "latest": "{}".format(self.LATEST),
+                   "oldest": "{}".format(self.OLDEST)}
         response = requests.get(url, params=payload)
         json_data = response.json()
         channel_message_list = []
@@ -49,49 +50,16 @@ class SlackAPI(object):
             })
         return channel_message_list
 
-    def post_message(self, message=None):
+    def post_message(self, message=None, attachments=None):
 
         url = "	https://slack.com/api/chat.postMessage"
         payload = {
             "token": self.BOT_USER_OAUTH_ACCESS_TOKEN,
             "channel": "#bot_test",
-            "text": "{}".format(message),
+            "text": "<!channel>\n*{}*".format(message),
+            "attachments": "{}".format(attachments),
         }
         response = requests.post(url, data=payload)
         print(response)
 
 
-if __name__ == "__main__":
-
-    import yaml
-    CONFIG_PATH = "./config.yaml"
-    with open(CONFIG_PATH) as f:
-        config = yaml.load(stream=f, Loader=yaml.SafeLoader)
-
-    slack_api = SlackAPI(
-        config["slack"][0]["oauth_access_token"],
-        config["slack"][0]["bot_user_oauth_access_token"]
-    )
-
-    """
-    channel_list = slack_api.get_channel_list()
-    member_list = slack_api.get_member_list()
-
-    for channel in channel_list:
-        print("============================")
-        print(channel["id"], channel["name"])
-        user_frequency_dict = {member["id"]: 0 for member in member_list}
-        user_frequency_dict["OTHERS"] = 0  # add other_user etc (rss...)
-        channel_message_list = slack_api.get_channel_message_list(
-            channel["id"], channel["name"])
-        print("message num: {}".format(len(channel_message_list)))
-
-        for channel_message in channel_message_list:
-            if not ("user" in channel_message["message"]):
-                user_frequency_dict["OTHERS"] += 1
-                continue
-            user_frequency_dict[channel_message["message"]["user"]] += 1
-
-        user_frequency_sorted = sorted(user_frequency_dict.items(), key=lambda x: x[1], reverse=True)
-        print(user_frequency_sorted)
-    """
