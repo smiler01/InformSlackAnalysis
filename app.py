83452c8882a677f777c18a3a35aefc16828e49c3
ubu18-kanji <smile.smiling01@gmail.com>
2019-07-21 02:09:24 +0900

    全体的に修正

--- app.py
+++ app.py
@@ -1,131 +1,127 @@
 # coding: utf-8
 import copy
 import yaml
-import time
-from datetime import datetime
+import logging.config
+import settings
+from slackapi import SlackAPI
 
-import slack_api
+LOGGING_CONF_PATH = "./log/debug.conf"
+logging.config.fileConfig(LOGGING_CONF_PATH)
+logger = logging.getLogger()
 
 CONFIG_PATH = "./config.yaml"
 with open(CONFIG_PATH) as f:
     config = yaml.load(stream=f, Loader=yaml.SafeLoader)
 
 
-def get_slack_analytics(order=1, channel_upper_limit=5, user_upper_limit=5):
+def format_user_ranking_attachments(users_dict):
 
-    """
-    :param order: 0(ascending) or 1(descending). If order =! 0 or 1, define order == 1.
-    :param channel_upper_limit: Number of channel information to acquire.
-    :param user_upper_limit: Number of user information to acquire.
-    :return: slack analytics of JSON format.
-    """
+    attachment = {"fallback": "This is slack user frequency"}
 
-    if order == 0:
-        order = False
-    elif order == 1:
-        order = True
-    else:
-        order = True
+    pretext = {"pretext": ":black_small_square:*最も投稿頻度の高いユーザーです*"}
+    attachment.update(pretext)
 
-    slack = slack_api.SlackAPI(
-        config["slack"]["oauth_access_token"],
-        config["slack"]["bot_user_oauth_access_token"])
+    fields = {"fields": []}
+    for rank, (user_id, user_value) in enumerate(users_dict):
+        fields["fields"].append(
+            {"value": "{}. {}: {} messages".format(rank + 1, user_value["name"], user_value["frequency"])})
+        if rank + 1 == config["displayed_number"]:
+            break
 
-    channel_list = slack.get_channel_list()
-    member_list = slack.get_member_list()
+    attachment.update(fields)
 
-    channel_analysis_list = []
-    for channel in channel_list:
-
-        # create member information list [{"id":***, "name":***, "frequency":***}, ...]
-        member_info_list = copy.deepcopy(member_list)
-        for member_info in member_info_list:
-            member_info.update({"frequency": 0})
-        member_info_list.append({"id": None, "name": "others", "frequency": 0})  # add "others" user
-
-        # add member user-post frequency
-        channel_message_list = slack.get_channel_message_list(channel["id"])
-        for channel_message in channel_message_list:
-            # if "user" not in message, add "others" frequency
-            if not ("user" in channel_message["message"]):
-                member_info_list[-1]["frequency"] += 1
-                continue
-            # add target-user frequency
-            for member_info in member_info_list:
-                if member_info["id"] == channel_message["message"]["user"]:
-                    member_info["frequency"] += 1
-
-        # sort user-post frequency
-        member_info_list_sorted = sorted(
-            member_info_list, key=lambda x: x["frequency"], reverse=order)
-
-        # mod channel_upper_limit
-        if len(member_info_list_sorted) < user_upper_limit:
-            user_upper_limit = len(member_info_list_sorted)
-        member_info_list_sorted = member_info_list_sorted[:user_upper_limit]
-
-        # get channel-frequency
-        channel_frequency = len(channel_message_list)
-
-        channel_analysis = {
-            "channel": channel["name"],
-            "channel_id": channel["id"],
-            "channel_frequency": channel_frequency,
-            "user_frequency": member_info_list_sorted}
-        channel_analysis_list.append(channel_analysis)
-
-    channel_analysis_list_sorted = sorted(channel_analysis_list, key=lambda x: x["channel_frequency"], reverse=order)
-
-    if len(channel_analysis_list_sorted) < channel_upper_limit:
-        channel_upper_limit = len(channel_analysis_list_sorted)
-
-    return channel_analysis_list_sorted[:channel_upper_limit]
+    return attachment
 
 
-def get_yesterday_datetime():
+def format_channel_ranking_attachments(channels_dict):
 
-    yesterday_timestamp = time.time() - 60*60*24
-    yesterday_datetime = datetime.fromtimestamp(yesterday_timestamp)
-    return "{0:%Y/%m/%d}".format(yesterday_datetime)
+    attachment_list = []
 
+    attachment = {"fallback": "This is channel frequency"}
+    pretext = {"pretext": ":black_small_square:*最も投稿頻度の高いチャンネルです*"}
+    attachment.update(pretext)
+    #attachment_list.append(attachment)
 
-def format_attachments(slack_analysis_information):
+    for channel_rank, (channel_id, channel_value) in enumerate(channels_dict):
 
-    attachments = []
+        # はじめのattachmentは上記のループ外のpretextと連動させることで、
+        # 空白行を埋めることができる。attachment_list.append()をコメントアウトしているのもそれに起因
+        if channel_rank != 0:
+            attachment = {"fallback": "This is channel frequency"}
 
-    for num, slack_analysis in enumerate(slack_analysis_information):
+        fields = {"fields": []}
+        fields["fields"].append(
+            {"value": "{}. {}: {} messages".format(
+                channel_rank + 1, channel_value["name"], channel_value["frequency"])})
 
-        attachment = {
-            "fallback": "This is rss reviews attachments",
-        }
+        for user_rank, (user_id, user_value) in enumerate(channel_value["users"]):
+            fields["fields"].append(
+                {"value": "・ {}: {} messages".format(user_value["name"], user_value["frequency"])})
+            if user_rank + 1 == config["displayed_number"]:
+                break
 
-        pretext_field = {"pretext": "*{}. <#{}|{}> : {} messages*".format(
-            num+1, slack_analysis["channel_id"], slack_analysis["channel"],
-            slack_analysis["channel_frequency"])}
-        attachment.update(pretext_field)
+        attachment.update(fields)
+        attachment_list.append(attachment)
 
-        fields_dict = {"fields": []}
-        for n, user_frequency in enumerate(slack_analysis["user_frequency"]):
-            fields_dict["fields"].append({"value": "{}. {}: {} message".format(
-                n+1, user_frequency["name"], user_frequency["frequency"]), "short": "true"})
+        if channel_rank + 1 == config["displayed_number"]:
+            break
 
-        attachment.update(fields_dict)
-
-        attachments.append(attachment)
-
-    return attachments
+    return attachment_list
 
 
 def main():
 
-    slack = slack_api.SlackAPI(
-        config["slack"]["oauth_access_token"],
-        config["slack"]["bot_user_oauth_access_token"])
+    slack = SlackAPI(
+        oauth_access_token=settings.OAUTH_ACCESS_TOKEN,
+        web_hook_url=settings.WEB_HOOK_URL,
+        day_terms=config["day_terms"]
+    )
 
-    slack_analysis_infomation = get_slack_analytics()
-    attachments = format_attachments(slack_analysis_infomation)
+    response = slack.get_users_list()
+    if response is False:
+        return False
+    user_dict_source = response["members"]
+    user_dict = {}
+    for user in user_dict_source:
+        if (user["deleted"] is False) and (user["is_bot"] is False):
+            user_dict.update({user["id"]: {"name": user["name"], "frequency": 0}})
 
-    message = "{}（昨日）のSlack内のアナリティクス情報をお届けします".format(get_yesterday_datetime())
+    response = slack.get_channels_list()
+    if response is False:
+        return False
+    channel_dict_source = response["channels"]
+    channel_dict = {}
+    for channel in channel_dict_source:
+        if (channel["is_archived"] is False) and (channel["is_private"] is False):
+            channel_dict.update(
+                {channel["id"]: {"name": channel["name"], "frequency": 0, "users": copy.deepcopy(user_dict)}})
+
+    for channel_id, channel_value in channel_dict.items():
+
+        response = slack.get_channels_history(channel_id)
+        messages = response["messages"]
+
+        for message in messages:
+
+            channel_users = channel_value["users"]
+
+            if "user" in message:
+                message_user_id = message["user"]
+                channel_users[message_user_id]["frequency"] += 1
+                user_dict[message_user_id]["frequency"] += 1
+
+        channel_value["users"] = sorted(channel_value["users"].items(), key=lambda x: x[1]["frequency"], reverse=True)
+        channel_value["frequency"] = len(messages)
+
+    channel_dict_desc = sorted(channel_dict.items(), key=lambda x: x[1]["frequency"], reverse=True)
+    user_dict_desc = sorted(user_dict.items(), key=lambda x: x[1]["frequency"], reverse=True)
+
+    message = ":looking: 先週のSlack内の活動頻度をお届けします"
+    attachments = []
+    attachment = format_user_ranking_attachments(user_dict_desc)
+    attachments.append(attachment)
+    attachment_list = format_channel_ranking_attachments(channel_dict_desc)
+    attachments.extend(attachment_list)
 
     slack.post_message(message=message, attachments=attachments)
 
