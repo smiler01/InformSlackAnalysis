5f8bcbede4c8ffecd75d621f5cde68ad6cedb1b1
ubu18-kanji <smile.smiling01@gmail.com>
2019-07-21 02:10:18 +0900

    環境変数を追加

--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
+OAUTH_ACCESS_TOKEN = os.environ.get("OAUTH_ACCESS_TOKEN")
+WEB_HOOK_URL = os.environ.get("WEB_HOOK_URL")
